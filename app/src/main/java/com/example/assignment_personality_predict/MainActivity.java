a7edcf83f958fcfc02bcaf54d737720d1d055fa1
Duy An Ngo Nguyen <40843946+ZoroZero@users.noreply.github.com>
2020-11-21 19:43:04 +0700

    Init android app

---
+++ app/src/main/java/com/example/assignment_personality_predict/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.assignment_personality_predict;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
